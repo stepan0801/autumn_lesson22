5af8dc1b3752aab96eac14a0b6c26cf4337c6d06
stepan0801 <98427795+stepan0801@users.noreply.github.com>
2022-11-29 19:32:17 +0200

    new

---
+++ js/script.js
@@ -0,0 +1,16 @@
+`use strict`
+console.log(`Я вивчаю JS`);
+// let user;
+// let userName;
+// userName = "Вася";
+// user = userName;
+// console.log(user)
+// if (true) {
+// 	var size = 15;
+// }
+// console.log(size)
+// let userAge = 36;
+// let userInfo = "Фрілансер ${userAge}";
+// console.log(userInfo);
+// let userSize = "45" / "8"
+// console.log(typeof userSize)
